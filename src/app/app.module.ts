c7bf88b2cf7fff228504cf4025e03caf88af6c02
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-02 13:31:11 -0300

    Splash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,14 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"primer-parcial-app1","appId":"1:915380151213:web:e25ef4b9543b87809e7724","storageBucket":"primer-parcial-app1.appspot.com","locationId":"southamerica-east1","apiKey":"AIzaSyBiLy7FGCZADs_CuYXVrsnUUvmT8sWfXtc","authDomain":"primer-parcial-app1.firebaseapp.com","messagingSenderId":"915380151213"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({
+    "projectId":"primer-parcial-app1",
+    "appId":"1:915380151213:web:e25ef4b9543b87809e7724",
+    "storageBucket":"primer-parcial-app1.appspot.com",
+    "apiKey":"AIzaSyBiLy7FGCZADs_CuYXVrsnUUvmT8sWfXtc",
+    "authDomain":"primer-parcial-app1.firebaseapp.com",
+    "messagingSenderId":"915380151213"
+  })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
