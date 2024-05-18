cdb298bd0c9dab5bf22f87252d9a1dbb8f5cb8a2
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-18 11:12:41 -0300

    Splash screen plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pp3.app',
   appName: 'PPS-PP-App3',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      launchFadeOutDuration: 500,
+      launchShowDuration: 1000,
+      splashFullScreen: false
+    }
+  }
 };
 
 export default config;
